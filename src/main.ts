506b15c31bab9c88dc0a82a57867ef4c2ef34648
Justin Tillman <tillman.55@buckeyemail.osu.edu>
2019-03-19 23:28:07 -0400

    starting heroku support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import * as env from 'dotenv';
 
 async function bootstrap() {
+  // env.config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
